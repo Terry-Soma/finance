c09cfc0601328c82a288759e2386e707cfb0ce58
Terry <jackhack116@yahoo.com>
2021-03-26 17:36:08 +0800

    orlogo zarlagaas tusviig boddog bolgov

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var uiCtrl = (function ()
         inpType : ".add__type",
         inpDescription : ".add__description",
         inpValue : ".add__value",
-        addBtn : ".add__btn"
+        addBtn : ".add__btn",
+        incomeList : '.income__list',
+        expenseList : '.expenses__list'
 
 
 
@@ -15,11 +17,42 @@ var uiCtrl = (function ()
             return{
                 type: document.querySelector(DOMstr.inpType).value,
                 description: document.querySelector(DOMstr.inpDescription).value,
-                value : document.querySelector(DOMstr.inpValue).value
-            }
+                value :parseInt(document.querySelector(DOMstr.inpValue).value)
+            };
         },
         getDomstr : function(){
             return DOMstr;
+        },
+        clearFields: function(){
+            var fields = document.querySelectorAll(DOMstr.inpDescription+ ", "+ DOMstr.inpValue);
+// list to array
+            var fieldsArr = Array.prototype.slice.call(fields);
+            fieldsArr.forEach(function(el, index,array){
+                el.value ="";
+                
+            });
+            fieldsArr[0].focus();
+
+        },
+
+        addListItem: function(item,type){
+            // inc or exp 
+            var html,list;
+            if(type ==='inc'){
+                list = DOMstr.incomeList;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">$Description$</div><div class="right clearfix"><div class="item__value">$value$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div></div></div>';
+
+            }else{
+                list = DOMstr.addBtn.expenseList;
+                html = ' <div class="item clearfix" id="expense-%id%"><div class="item__description">$Description$</div><div class="right clearfix"><div class="item__value">$value$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            html= html.replace('%id%',item.id);
+            html = html.replace('$Description$',item.description);
+            html = html.replace('$value$',item.value);
+
+            document.querySelector(list).insertAdjacentHTML('beforeend',html);
+
+
         }
 
    };
@@ -38,6 +71,15 @@ var financeCtrl = (function (){
         this.description = description;
         this.value = value;
     };
+
+    var calculateTotal = function(type){
+       var sum = 0;
+        data.allItems[type].forEach(function(el){
+            sum += el.value;
+
+        });
+        data.totals[type] =sum;
+    }
     var data = {
         allItems: {
             inc: [],
@@ -46,7 +88,9 @@ var financeCtrl = (function (){
         totals: {
             inc:0,
             exp: 0
-        }
+        },
+        tusuv : 0,
+        huvi: 0
     };
     return{
         addItem: function(type,description,value){
@@ -65,21 +109,59 @@ var financeCtrl = (function (){
            }
             data.allItems[type].push(item);
 
+            return item;
 
 
 
+
+        },
+
+        Analyze : function(){
+            calculateTotal('inc');
+            calculateTotal('exp');
+            data.tusuv = data.totals.inc - data.totals.exp;
+
+            data.huvi = Math.round((data.totals.exp / data.totals.inc)*100);
+        },
+
+        Display: function(){
+            return {
+                tusuv : data.tusuv,
+                huvi : data.huvi,
+                totalInc : data.totals.inc,
+                totalExp : data.totals.exp
+
+            };
+
+        },
+        seeData:function(){
+            return data;
         }
-    }
+    };
 
 })();
 
 var appCtrl = (function (uiC, fnC) {
     var crtlAddItem = function () {
 
+        
     var inp = uiCtrl.getInput();
-    console.log(inp);
-        financeCtrl.addItem(inp.type,inp.description,inp.value);
-};
+    if(inp.description !== "" && inp.value !== ""){
+
+     var item = financeCtrl.addItem(inp.type,inp.description,inp.value);
+     console.log(inp);
+     uiCtrl.addListItem(item,inp.type);
+     uiCtrl.clearFields();
+
+
+     financeCtrl.Analyze();
+
+    var tusuv =  financeCtrl.Display();
+    console.log(tusuv);
+
+
+    }
+ };
 var setupEventListeners= function(){
     
 var Dom = uiCtrl.getDomstr();
